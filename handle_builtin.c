fe078ba202fe4a1a52c5cd986c576cd80a5e39ce
xl-c111 <10430@holbertonstudents.com>
2025-04-22 13:46:26 +0000

    merge handle_builtin.c from Xiaoling_Cui

---
+++ handle_builtin.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include "shell.h"
+
+/**
+ * handle_builtin - checks and hnadles built-in commands: exit and env
+ * @argv: array of command arguments
+ *
+ * Return: 1 if the built-in command is "exit" and shell should exit
+ *         otherwise 0 (including built-in command "env" is executed)
+ *
+ */
+int handle_builtin(char *argv[])
+{
+	int i;
+
+	if (argv == NULL)
+		return (0);
+
+	if (strcmp(argv[0], "exit") == 0)
+		return (1);
+
+	if (strcmp(argv[0], "env") == 0)
+	{
+		for (i = 0; environ[i] != NULL; i++)
+			printf("%s\n", environ[i]);
+		return (0);
+	}
+	return (0);
+}
