c6672101b6740561f7556f4d2ad33573112299e2
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-24 03:02:50 +1000

    Update _getenv.c
    - strncmp(environ[i], name, len) to compare the first len chars in environ[i] with name
     - environ[i][len] == '=' to check if the char after name is '=', which is the whole variable name 
     - return (&environ[i][len + 1]) returns the char after '=', which is the start of variable's value string

--- _getenv.c
+++ _getenv.c
@@ -6,6 +6,10 @@
  * @name: a ptr to the given name of the environment variable to search for
  *
  * Return: ptr to the value string, NULL if not found
+ * Description: - strncmp(environ[i], name, len) to compare the first len chars in environ[i] with name
+ *              - environ[i][len] == '=' to check if the char after name is '=', which is the whole variable name 
+ *              - return (&environ[i][len + 1]) returns the char after '=', which is the start of variable's value string
+ *                If environ[i] is "PATH=/usr/bin", then environ[i][len + 1] is '/', pointing to "/usr/bin".
  */
 
 char *_getenv(const char *name)
@@ -18,9 +22,10 @@ char *_getenv(const char *name)
 
 	while (environ[i] != NULL)
 	{
+		/* strncmp to compare the first len chars of enbiron[i] with name and check if the next char after name is '='*/
 		if (strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
 		{
-			return (&environ[i][len + 1]); /* return the ptr to the value of string */
+			return (&environ[i][len + 1]);      /* return the ptr to the char after '=' in environ[i] */
 		}
 		i++;
 	}
