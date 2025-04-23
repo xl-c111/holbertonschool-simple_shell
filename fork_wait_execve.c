506577be63630189ed0c73e0d034fdbece501d78
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-24 00:57:55 +1000

    add comments
    execve(command_path, argv, environ): to replace the current process(child process) with a new program stored in command_path, the parent process remains unchanged and wait for the child process to finish
    int waitpid(pid, raw_status, options): wait pid to finishes, once finishes, the parent resumes and stores the child's exit status in raw_status

--- fork_wait_execve.c
+++ fork_wait_execve.c
@@ -8,37 +8,54 @@
 #include "shell.h"
 /**
  * fork_wait_execve - forks a child process to execute external commands
- * @argv: array of strings representing command and its arguments
- * @command_path: full path to the command
- * @raw_status: a pointer to the raw exit status of the child process
+ * @argv: list of arguments( including command and its arguments)
+ *        - it is passesd to execve as the second argument, telling system how to execute it
+ *        - argv[0] is the program name, which equals to basename(command_path), argv[i] are program arguments
+ * @command_path: full path to the executable command
+*         - it is passed to execve as the first argument, telling system actual location of executable)
+ * @raw_status: a pointer to int where the raw exit status of child process will be stored 
  *
  * Return: in the child process, does not return if execve succeeds
  *         if execve fails, it prints an error and exits
  *         in the parent process, it wait for the child process to finish
+ * 
+ * Description: 1，execve(command_path, argv, environ)
+ *              - repplace the current(child) process with a new program(which is located in command_path)
+ *              - so the child process tansforms into a new program you want to run(like ls, gcc)
+ *              - the parent remains unchnaged until child process terminates
+ *              2， int waitpid(pid_t pid, int *status, int options);
+ *              - @pid: the process ID of child process
+ *              - @status: a ptr to an int where the exit status of child process will be stored, 
+ *                         it's a raw status, must be interpreted using macros like WIFEXITED() or WEXITSTATUS() to get the real exit code
+ *                         - WIFEXITED(status) to check if the child exits normally
+ *                         - WEXITSTATUS(status) to extract the exit code from raw status
+ *              - @options: 0 means "default behavior" — just wait for the child to terminate
  */
 
 void fork_wait_execve(char *argv[], char *command_path, int *raw_status)
 {
 	pid_t pid;
 
-	pid = fork();
+	pid = fork();          /* create a new process */
 
-	if (pid == -1)
+	if (pid == -1)         /* check if fork failed */
 	{
 		perror("fork");
-		free(command_path);
+		free(command_path);      /* clean up the memory */
 		return;
 	}
 
-	if (pid == 0)
+	if (pid == 0)          /* if in the child process */
 	{
-		execve(command_path, argv, environ);
-		perror(command_path);
-		exit(127);
+		/* replace the child process with a new program located in command_path */
+		/* if execve succeeds, the current process is replaced, non of following lines run */
+		execve(command_path, argv, environ);  
+		perror(command_path);       /* this line only runs when execve fails*/
+		exit(127);                  /* 127 means command not found */
 	}
-	else
+	else                                /* this block is executed in parent process */
 	{
-		waitpid(pid, raw_status, 0);
+		waitpid(pid, raw_status, 0);  /* wait for the child process to terminate, store its status in raw_status*/
 		free(command_path);
 	}
 }
